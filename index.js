44fa946b2beb488b01ec473de218c730523c4b37
pnaruka054 <pnaruka054@gmail.com>
2024-07-14 22:26:52 +0530

    dsgfsdfdsf

--- index.js
+++ index.js
@@ -9,7 +9,25 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
+app.get('/Verify', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
+app.get('/signUp', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
+app.get('/forgot-password', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
+app.get('/Home', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
+app.get('/UpdatePassword/:id', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
 
 async function modelCalled() {
     await mongoose.connect(process.env.DATABASE_URL)
